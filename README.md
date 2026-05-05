40d956cfec6ef5daa3ebe50452bd372712b84746
MarianaNietoSilva <mariananietodaw@gmail.com>
2026-05-05 10:13:34 +0200

    cambio 2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # EA-3.4-Proyectos-javascript
 Proyecto 1: El Tablero de Ajedrez Automático Objetivo: Crear un tablero de ajedrez funcional de 8x8 utilizando bucles anidados en JavaScript, evitando dibujar cada casilla manualmente en el HTML.
+/proyecto
+ ├── index.html
+ ├── style.css
+ └── script.js
--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//  CAMBIO DE PROYECTO
+function mostrarProyecto(id) {
+    document.getElementById("ajedrez").classList.add("oculto");
+    document.getElementById("juego").classList.add("oculto");
+
+    document.getElementById(id).classList.remove("oculto");
+}
+
+
+// ajedrez 
+
 const tablero = document.getElementById("tablero");
 
 // piezas iniciales
@@ -32,3 +43,43 @@ for (let fila = 0; fila < 8; fila++) {
         tablero.appendChild(casilla);
     }
 }
+
+// juego
+const personaje = document.getElementById("personaje");
+const obstaculo = document.getElementById("obstaculo");
+const scoreText = document.getElementById("score");
+const mensaje = document.getElementById("mensaje");
+
+let puntos = 0;
+let vivo = true;
+
+// salto
+document.addEventListener("keydown", function(e) {
+    if (e.code === "Space" && !personaje.classList.contains("saltar")) {
+        personaje.classList.add("saltar");
+
+        setTimeout(() => {
+            personaje.classList.remove("saltar");
+        }, 500);
+    }
+});
+
+// puntuación
+setInterval(() => {
+    if (vivo) {
+        puntos++;
+        scoreText.textContent = "Puntos: " + puntos;
+    }
+}, 1000);
+
+// colisión
+setInterval(() => {
+    const personajeBottom = parseInt(window.getComputedStyle(personaje).getPropertyValue("bottom"));
+    const obstaculoLeft = parseInt(window.getComputedStyle(obstaculo).getPropertyValue("left"));
+
+    if (obstaculoLeft < 70 && obstaculoLeft > 30 && personajeBottom < 40) {
+        mensaje.textContent = "¡Has perdido!";
+        vivo = false;
+        obstaculo.style.animation = "none";
+    }
+}, 50);
